fce94d11bc5168215339abd32c6ed498a5939bdb
Nick Lediet <nlediet@northern.co>
2021-06-16 12:55:48 -0400

    Finish snx install script

---
+++ snx-vpn.install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Notes:
+#
+#
+#
+#
+#
+#
+
+# Setup Temp directory
+mkdir temp && cd temp
+
+# Get the installation script
+wget https://starkers.keybase.pub/snx_install_linux30.sh?dl=1 -O snx_install.sh
+
+# Setup 32-bit architecture support for debian based distros (Debian, Ubuntu, Mint...)
+sudo dpkg --add-architecture i386
+sudo apt update 
+
+# Install 32-bit dependencies for building snx
+sudo apt install libstdc++5:i386 libx11-6:i386 libpam0g:i386
+
+# Run the installation script 'snx_install.sh'
+chmod a+rx snx_install.sh
+sudo ./snx_install.sh
+
+
+
+# Exit and remove the Temp directory
+cd .. && rm -rf temp/
+
